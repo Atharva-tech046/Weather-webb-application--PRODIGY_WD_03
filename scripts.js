317ce6e17d892ec774ca10a9122134add723879d
Atharva Mohite <atharvam2017@gmail.com>
2025-02-11 16:03:45 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function getWeather() {
+    const apiKey = "f4d3533447701788abe16e91b978ac83"; 
+    const city = document.getElementById("city").value;
+    
+    if (city === "") {
+        alert("Please enter a city name.");
+        return;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("city-name").innerText = data.name;
+            document.getElementById("temperature").innerText = `Temperature: ${data.main.temp}°C`;
+            document.getElementById("description").innerText = data.weather[0].description;
+            document.getElementById("icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+        })
+        .catch(error => alert("City not found!!"));
+}
